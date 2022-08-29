1dec5d112d3e352ab557eb4a499060378afd212f
ryz-code <ryjprkt@gmail.com>
2022-08-29 08:05:03 +0700

    update ginkgo [skip ci]

--- sync.sh
+++ sync.sh
@@ -4,13 +4,13 @@ git config --global user.name "$USER_NAME"
 git config --global user.email "$USER_EMAIL"
  
 # Kernel Sources!
-git clone --depth=1 https://github.com/Kyvangka1610/android_kernel_xiaomi_ginkgo -b Q-OLD $CIRRUS_WORKING_DIR/KERNEL
+git clone --depth=1 https://github.com/Kyvangka1610/android_kernel_xiaomi_ginkgo $CIRRUS_WORKING_DIR/KERNEL
 
 # Tool Chain
 # Proton Clang ---
  git clone --depth=1 https://github.com/kdrag0n/proton-clang $CIRRUS_WORKING_DIR/PROTON-CLANG
 # Azure Clang ---
-# git clone --depth=1 https://gitlab.com/Panchajanya1999/azure-clang $CIRRUS_WORKING_DIR/AZURE-CLANG
+ git clone --depth=1 https://gitlab.com/Panchajanya1999/azure-clang $CIRRUS_WORKING_DIR/AZURE-CLANG
 # Xrage Clang ---
 # git clone --depth=1 https://github.com/xyz-prjkt/xRageTC-clang $CIRRUS_WORKING_DIR/XRAGE-CLANG
 # Greenforce Clang ---
